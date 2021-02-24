8e8180c87db0bcf08202a8891a9badf15ba51bac
Karolina <k99zielonka@gmail.com>
2021-02-24 18:34:54 +0100

    Initial commit

---
+++ todo/tasks/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import *
+
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Dodaj nowe zadanie'}))
+    class Meta:
+        model = Task
+        fields = '__all__'
